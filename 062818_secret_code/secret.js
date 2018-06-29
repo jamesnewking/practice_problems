512f92b8630cb60b3836a72c9e27085eb1ee8b2d
James Wang <jamesnewking@gmail.com>
2018-06-28 22:17:32 -0700

    created dynamically object of arrays

--- 062818_secret_code/secret.js
+++ 062818_secret_code/secret.js
@@ -3,20 +3,17 @@ const inString = 'help we have been spotted';
 
 function secret(readable){
     readable = readable.replace(/\s/g,'');
-    const loops = (readable.length/6) + 1;
-    const outArr = [];
-    const output = {0:[],1:[],2:[],3:[]};
+    const output = {};//0:[],1:[],2:[],3:[]
     let flip = true;
     let counter = 0;
     for (let index = 0;index < readable.length; index++){
-        debugger;
-
-
         if(flip){
+            if(!(output[counter])){output[counter]=[]};
             output[counter].push(readable[index]);
             counter++;
             if(counter===3){flip=false};
         }else{
+            if(!(output[counter])){output[counter]=[]};
             output[counter].push(readable[index])
             counter--;
             if(counter===0){flip=true};
